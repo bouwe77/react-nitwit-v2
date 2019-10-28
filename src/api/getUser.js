fc7f3d4a3f3eb36d859f18f9bebdc4419d6d6d3b
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-10-28 20:49:27 +0100

    .

--- src/api/getUser.js
+++ src/api/getUser.js
@@ -4,6 +4,8 @@ import * as jwt from "../auth/jwt";
 export function getCurrentLoggedInUser() {
   const token = jwt.get();
 
+  if (!token) return Promise.resolve(null);
+
   const url = `${process.env.REACT_APP_API_URL}/users/whoami`;
-  return token ? get(url, token) : null;
+  return get(url, token);
 }
